59e328955bef0cbf2a52c7b2a1bd2fd509515550
Aiden Cooper <aidencooper@Aidens-MacBook-Pro.local>
2021-01-06 06:41:23 +0000

    Chapter 19: Add twitter field

--- src/DataFixtures/UserFixture.php
+++ src/DataFixtures/UserFixture.php
@@ -26,11 +26,26 @@ class UserFixture extends BaseFixture
            $user = new User();
            $user->setEmail(sprintf("spacebar%d@example.com",$i));
            $user->setFirstName($this->faker->firstName);
+           if($this->faker->bool) {
+               $user->setTwitterUsername($this->faker->userName);
+           }
            $user->setPassword($this->encoder->encodePassword(
                $user,
                'engage'
            ));
            return $user;
+        });
+
+        $this->createMany(3, 'main_users', function($i){
+            $user = new User();
+            $user->setEmail(sprintf("admin%d@email.com",$i));
+            $user->setFirstName($this->faker->firstName);
+            $user->setRoles(['ROLE_ADMIN']);
+            $user->setPassword($this->encoder->encodePassword(
+                $user,
+                'engage'
+            ));
+            return $user;
         });
 
         $manager->flush();
